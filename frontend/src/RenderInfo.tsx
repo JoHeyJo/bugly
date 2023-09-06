b8690477cc18425e8759a009fdd0ee0eeed0b567
jf <jpf0628@gmail.com>
2023-09-05 17:38:49 -0700

    added logic to handle swtiching between project info

--- frontend/src/RenderInfo.tsx
+++ frontend/src/RenderInfo.tsx
@@ -12,48 +12,56 @@ import './style/RenderInfo.css';
 type InfoProp = {
   projectData: IDetailData
 }
+
+type Section = {
+  [key: string]: boolean;
+  details: boolean;
+  tech: boolean;
+  specs: boolean;
+}
+
+const defaultSections = { 'details': false, 'tech': false, 'specs': false }
 /** Renders project's specs, tech, details 
  * DetailsSlidOver -> Info
 */
 function RenderInfo(data: InfoProp) {
-  const [section, setSection] = useState({ 'details': false, 'tech': false, 'specs': false })
+  const [section, setSection] = useState<Section>(defaultSections)
 
   const { projectId } = useContext(ProjectContext)
 
   /** updates state with selection section */
-  function selectSection(e:any) {
+  function selectSection(e: any) {
     e.preventDefault()
-    const section = e.target.name;
-    console.log('section', section)
-    setSection(section =>{
-      const toggledSection = {...section}
-      return section
+    const selectedSection = e.target.name;
+    setSection(section => {
+      section = defaultSections;
+      const toggledSection = { ...section, [selectedSection]: true }
+      return toggledSection
     })
   }
 
   return (
     <>
       <span id="RenderInfo-add">
-        <h3 className="RenderInfo-sections">
+        <h3 className={section.details ? "RenderInfo-sections active" : "RenderInfo-sections"}>
           <button name="details"
             onClick={selectSection} className="RenderInfo-buttons">Details
           </button>
         </h3>
-        <h3 className="RenderInfo-sections">
+        <h3 className={section.tech ? "RenderInfo-sections active" : "RenderInfo-sections"}>
           <button name="tech"
             onClick={selectSection} className="RenderInfo-buttons">Tech
           </button>
         </h3>
-        <h3 className="RenderInfo-sections">
+        <h3 className={section.specs ? "RenderInfo-sections active" : "RenderInfo-sections"}>
           <button name="specs"
             onClick={selectSection} className="RenderInfo-buttons">Specs
           </button>
         </h3>
         <PopOut id={projectId} action={"new info"} getProject={() => { }} postId={undefined} />
       </span>
-      <DetailsList list={data.projectData.details} />
-      {/* <TechList /> */}
-
+      {section.details && <DetailsList list={data.projectData.details} />}
+      {section.tech && <TechList />}
     </>
   )
 }
