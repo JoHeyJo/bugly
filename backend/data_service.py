93abae651565298101a548490dd457fa289ee61c
jf <jpf0628@gmail.com>
2023-10-23 11:08:10 -0700

    data_service handles spec data. Spec included in return object

--- backend/data_service.py
+++ backend/data_service.py
@@ -1,15 +1,16 @@
-from models import User, Post, Tech, Detail, ProjectTech, Project
+from models import User, Post, Tech, Detail, ProjectTech, Project, Spec
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
 
 
 def tech_and_detail(db, project_id):
-    """Join data from two separate tables that don't reference each other"""
-    detail_data = db.session.query(Detail.detail).filter(Detail.project_id == project_id).all()
-    tech_data = db.session.query(Tech.tech, Tech.id).join(ProjectTech).join(Project).filter(Project.id == project_id).all()
+    """Join data from multiple tables that don't directly reference each other"""
+    spec_data = db.session.query(Spec.spec).filter(Spec.project_id==project_id).all()
+    detail_data = db.session.query(Detail.detail).filter(Detail.project_id==project_id).all()
+    tech_data = db.session.query(Tech.tech, Tech.id).join(ProjectTech).join(Project).filter(Project.id==project_id).all()
     combined_data = {
         'details': [detail[0] for detail in detail_data],
-        # 'tech': [tech[0] for tech in tech_data]
+        'spec': [spec[0] for spec in spec_data],
         'tech': [{'id': tech[1], 'tech': tech[0]} for tech in tech_data]
     }
 
