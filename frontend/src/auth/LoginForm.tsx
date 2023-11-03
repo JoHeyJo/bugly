de50c9a0dcb064c6f11a5b330a7b9884a6208cd6
jf <jpf0628@gmail.com>
2023-11-02 22:18:02 -0700

    code clean up

--- frontend/src/auth/LoginForm.tsx
+++ frontend/src/auth/LoginForm.tsx
@@ -51,52 +51,6 @@ function LoginForm({ login }:any) {
   }
 
   return (
-    // <div id="LoginForm">
-    //   <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-    //     <h3 className="mb-3">Log In</h3>
-
-    //     <div className="card">
-    //       <div className="card-body-loginForm">
-    //         <form onSubmit={handleSubmit}>
-    //           <div className="mb-3">
-    //             <label className="form-label">Username</label>
-    //             <input
-    //               name="email"
-    //               className="LoginForm-form-control"
-    //               value={formData.email}
-    //               onChange={handleChange}
-    //               autoComplete="email"
-    //               required
-    //             />
-    //           </div>
-    //           <div className="mb-3">
-    //             <label className="form-label">Password</label>
-    //             <input
-    //               type="password"
-    //               name="password"
-    //               className="LoginForm-form-control"
-    //               value={formData.password}
-    //               onChange={handleChange}
-    //               autoComplete="current-password"
-    //               required
-    //             />
-    //           </div>
-
-    //           {formErrors.length
-    //             ? <AlertPopUp variant={"danger"} message={formErrors} />
-    //             : null}
-
-    //           <div className="d-grid">
-    //             <button className="btn btn-outline-primary" onClick={handleSubmit}>
-    //               Submit
-    //             </button>
-    //           </div>
-
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div id="LoginForm" style={{height: "75vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
       <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
         <h3 className="mb-3">Log In</h3>
